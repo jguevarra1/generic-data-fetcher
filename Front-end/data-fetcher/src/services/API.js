9dd63ccc20392e1e49226f44a5280aab6f13143b
Josh Guevarra <joshuaguevarra@gmail.com>
2023-06-09 11:58:54 -0700

    Revert "initial commit"
    This reverts commit 9a37c89ece37c738d1ceb8cb436c79251f3959be.

--- Front-end/data-fetcher/src/services/API.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default(url='http://localhost:8080') => {
-    return axios.create({
-        baseURL: url,
-        headers: {'Content-Type' : 'application/json', 'charset' : 'UTF-8'},
-    })
-}
